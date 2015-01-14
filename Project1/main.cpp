13e56052cfb3a37b2e6cf0eabd7063ad05703e01
ben-freke <b.freke@lancaster.ac.uk>
2015-01-14 19:35:05 +0000

    Triangle placed into Class/Object Structure

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,6 +2,51 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+class Triangle{
+public:
+	GLuint vaoid;
+	GLuint vboid;
+
+
+	int initSquare(){
+		vaoid = 6;
+		glGenVertexArrays(1, &vaoid);
+		glBindVertexArray(vaoid);
+		//Tells OpenGL that we are going to be using the vaoid array
+
+		static const GLfloat verts[] = {
+			//X, Y, Z
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f, 0.5f, 0.0f
+
+		};
+		//This is an array of vertificies
+		//Contains the array for the coordinates for the triange
+
+		
+		glGenBuffers(1, &vboid);
+		glBindBuffer(GL_ARRAY_BUFFER, vboid);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+		return 0;
+
+	}
+	int drawSquare(){
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vboid);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+		glDisableVertexAttribArray(0);
+
+		return 0;
+	}
+
+};
+
 
 
 int main(void)
@@ -31,28 +76,11 @@ int main(void)
 		return -1;
 	}
 
-	GLuint vaoid = 6;
-	glGenVertexArrays(1, &vaoid);
-	glBindVertexArray(vaoid);
-	//Tells OpenGL that we are going to be using the vaoid array
-
-	static const GLfloat verts[] = {
-		//X, Y, Z
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-
-	};
-	//This is an array of vertificies
-	//Contains the array for the coordinates for the triange
-		
-	GLuint vboid;
-	glGenBuffers(1, &vboid);
-	glBindBuffer(GL_ARRAY_BUFFER, vboid);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	
 
 
-
+	Triangle myTriange;
+	myTriange.initSquare();
 
 
 	/* Loop until the user closes the window */
@@ -61,14 +89,7 @@ int main(void)
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vboid);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		
-
-		glDisableVertexAttribArray(0);
+		myTriange.drawSquare();
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
